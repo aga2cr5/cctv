af56c51a8ea0c9615789092b55c1c67d54e9d2c0
aga2cr5 <kodak2cr5@gmail.com>
2023-04-14 18:38:16 +0300

    added things mattermost webhook stuff

--- webcam.py
+++ webcam.py
@@ -4,19 +4,54 @@ from flask import Flask, Response
 import cv2
 import time
 import datetime
+import subprocess
+import requests
+import json
+
+
+SECONDS_TO_RECORD_AFTER_DETECTION = 10
+TIME_TO_START_FILMING_AFTER_DOOR_OPENING = 1800
+MATTERMOST_WEBHOOK = "https://chat.entropy.fi/hooks/ifqmnphi13rtpqdi65fe9jjw8h"
+
 
 app = Flask(__name__)
-cap = cv2.VideoCapture(4)
+cap = cv2.VideoCapture(0)
 
 if not cap.isOpened():
     print("Cannot open camera")
     exit()
 mog = cv2.createBackgroundSubtractorMOG2()
 
-SECONDS_TO_RECORD_AFTER_DETECTION = 5
 
 frame_size = (int(cap.get(3)), int(cap.get(4)))
 fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+
+
+def send_notification_to_mattermost(url, message):
+    default_headers = {"content-type": "application/json"}
+    requests.post(url, data=json.dumps(message), headers=default_headers)
+
+
+def get_door_log():
+    try:
+        response = subprocess.run(
+            "ssh cctv@192.168.1.20 tail -n 1 /home/av/electric_door_log.txt",
+            shell=True,
+            capture_output=True,
+        )
+        time_in_unix = int(response.stdout.strip().split(b" ")[-1])
+        return time_in_unix
+
+    except Exception as err:
+        message = err.message
+        send_notification_to_mattermost(MATTERMOST_WEBHOOK, message)
+    # if this fails then it says that the door has been opened like 30 mins before
+    return time.time() - 1800
+
+
+def process_image(frame):
+    """Move the image processing stuff here from the gen method"""
+    pass
 
 
 def gen(cap):
@@ -41,15 +76,23 @@ def gen(cap):
 
         # tässä voisi vielä miettiä haluaako jotenkin filtteröidä noita liikehavaintoja
         # if cv2.contourArea(contour) < 2000:
+        moving_targets = list(
+            filter(lambda contour: cv2.contourArea(contour) > 2000, contours)
+        )
+        # aikaisemmin tässä oli vain contours moving_targets sijasta
+        if len(moving_targets) > 0:
+            time_now = time.time()
+            last_login = get_door_log()
 
-        if len(contours) > 0:
             if detection:
                 timer_started = False
-            else:
+            elif time_now - last_login > TIME_TO_START_FILMING_AFTER_DOOR_OPENING:
                 detection = True
                 current_time = datetime.datetime.now().strftime("%d-%m-%Y-%H-%M-%S")
                 out = cv2.VideoWriter(f"{current_time}.mp4", fourcc, 20, frame_size)
-                print("Started Recording!")
+                message = "Started Recording!"
+                send_notification_to_mattermost(MATTERMOST_WEBHOOK, message)
+                print(message)
         elif detection:
             if timer_started:
                 if (
